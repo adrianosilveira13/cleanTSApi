b1d21c675cc0d30e69e5669d1349386b59b6f1c8
Adriano <kdr@outlook.com.br>
2022-03-28 10:48:03 -0300

    test: ensure ValidationComposite not return if Validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -41,11 +41,9 @@ describe('Validation Composite', () => {
     expect(error).toEqual(new Error())
   })
 
-  it('Should not return the first error if more then one validation fails', () => {
-    const { sut, validationStubs } = makeSut()
-    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
-    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+  it('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
     const error = sut.validate({ field: 'any_value' })
-    expect(error).toEqual(new Error())
+    expect(error).toBeFalsy()
   })
 })
