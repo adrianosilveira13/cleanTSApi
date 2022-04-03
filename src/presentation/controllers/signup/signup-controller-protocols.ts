4e809740632ce49443251b9c9f6a6819f80e5efa
Adriano <kdr@outlook.com.br>
2022-04-03 17:19:13 -0300

    refactor: move validation to its own layer

--- src/presentation/controllers/signup/signup-controller-protocols.ts
+++ src/presentation/controllers/signup/signup-controller-protocols.ts
@@ -1,6 +1,5 @@
-export * from '../../protocols/email-validator'
+export * from '../../../validation/protocols/email-validator'
 export * from '../../protocols'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account-model'
-export * from '../../protocols/validation'
 export * from '../../../domain/usecases/authentication'
