f9d42079e5ae5b5fe76a56d1668aad13ace2f474
Adriano <kdr@outlook.com.br>
2022-04-07 09:34:42 -0300

    test: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -20,7 +20,7 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    it('Should return 204 on add survey success', async () => {
+    it('Should return 403 on add survey without access token', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -31,7 +31,7 @@ describe('Survey Routes', () => {
           }]
         }
         )
-        .expect(204)
+        .expect(403)
     })
   })
 })
