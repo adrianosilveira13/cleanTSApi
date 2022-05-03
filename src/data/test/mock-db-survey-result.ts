251f5a47bdc8ea04c2422086c5a246e0db5d70ec
Adriano <kdr@outlook.com.br>
2022-05-03 16:59:57 -0300

    refactor: reorganize folders

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -1,4 +1,4 @@
-import { SaveSurveyResultRepository } from '@/data/usecases/save-survey-result/db-save-survey-result-protocols'
+import { SaveSurveyResultRepository } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
 import { SurveyResultModel } from '@/domain/models/survey-result'
 import { mockSurveyResultModel } from '@/domain/test/mock-survey-result'
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-results/save-survey-result'
