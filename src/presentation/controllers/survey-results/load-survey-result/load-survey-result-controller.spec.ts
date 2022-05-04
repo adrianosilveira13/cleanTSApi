2a5cbc45636baf81090948e612a3a3d1ce571fd7
Adriano <kdr@outlook.com.br>
2022-05-04 11:12:30 -0300

    test: ensure LoadSurveyResultController returns 403 if invalid survey id is provided

--- src/presentation/controllers/survey-results/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-results/load-survey-result/load-survey-result-controller.spec.ts
@@ -2,7 +2,9 @@ import { LoadSurveyResultController } from './load-survey-result-controller'
 import {
   HttpRequest,
   mockLoadSurveyById,
-  LoadSurveyById
+  LoadSurveyById,
+  forbbiden,
+  InvalidParamError
 } from './load-survey-result-controller-protocols'
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -31,5 +33,12 @@ describe('LoadSurveyResult Controller', () => {
     const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
     await sut.handle(makeFakeRequest())
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  it('Should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(forbbiden(new InvalidParamError('surveyId')))
   })
 })
