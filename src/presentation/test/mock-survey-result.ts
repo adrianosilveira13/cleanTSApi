69481e96e088f499dc9d53e18204768958fa24eb
Adriano <kdr@outlook.com.br>
2022-05-04 11:31:55 -0300

    feat: LoadSurveyResultController calls LoadSurveyResult with correct value

--- src/presentation/test/mock-survey-result.ts
+++ src/presentation/test/mock-survey-result.ts
@@ -2,6 +2,7 @@ import { SaveSurveyResult } from '@/presentation/controllers/survey-results/save
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-results/save-survey-result'
 import { SurveyResultModel } from '@/domain/models/survey-result'
 import { mockSurveyResultModel } from '@/domain/test/mock-survey-result'
+import { LoadSurveyResult } from '@/domain/usecases/survey-results/load-survey-result'
 
 export const mockSaveSurveyResult = (): SaveSurveyResult => {
   class SaveSurveyResultStub implements SaveSurveyResult {
@@ -10,4 +11,13 @@ export const mockSaveSurveyResult = (): SaveSurveyResult => {
     }
   }
   return new SaveSurveyResultStub()
+}
+
+export const mockLoadSurveyResult = (): LoadSurveyResult => {
+  class LoadSurveyResultStub implements LoadSurveyResult {
+    load (surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResultModel())
+    }
+  }
+  return new LoadSurveyResultStub()
 }
